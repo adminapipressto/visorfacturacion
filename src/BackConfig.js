d431bc0610e5709defe398ecccddfe5cf77640a5
Pressto <adminapi@presstoperu.com>
2020-12-23 23:11:08 -0500

    Config BAck

--- src/BackConfig.js
+++ src/BackConfig.js
@@ -1,5 +1,5 @@
-// export const URL_BACK = 'https://evisorpressto.herokuapp.com/API/';
-export const URL_BACK = 'http://127.0.0.1:8000/API/';
+export const URL_BACK = 'https://evisorpressto.herokuapp.com/API/';
+// export const URL_BACK = 'http://127.0.0.1:8000/API/';
 
 export const FetchGet = async (endpoint) => {
     const response = await fetch(URL_BACK + endpoint);
